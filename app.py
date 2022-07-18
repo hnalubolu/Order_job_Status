ba8be64426ed1222a641bc832aff7f9d145e0cc0
Hareesh <68393761+hnalubolu@users.noreply.github.com>
2022-07-18 21:02:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import streamlit.components.v1 as components
 import matplotlib.pyplot as plt
 import plotly.express as px
 import seaborn as sns
-from st_aggrid import AgGrid,GridUpdateMode, JsCode
+from st_aggrid import AgGrid,GridUpdateMode,JsCode
 from st_aggrid.grid_options_builder import GridOptionsBuilder
 
 st.set_page_config(layout="wide")
